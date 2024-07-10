b4f44912789c928cf4f151a07051e8f6afd8d20a
LueKely <luekely43110@gmail.com>
2024-07-10 15:25:18 +0800

    miss me with that bullshit

--- src/Failures/TwoSum.ts
+++ src/Failures/TwoSum.ts
@@ -1,5 +1,11 @@
 export default {
-	twoSum() {
-		console.log('This is two sum nigga');
+	twoSum(nums: number[], target: number) {
+		for (let i = 0; i < nums.length; i++) {
+			for (let j = i + 1; j < nums.length; j++) {
+				if (nums[j] == target - nums[i]) {
+					return [i, j];
+				}
+			}
+		}
 	},
 };
